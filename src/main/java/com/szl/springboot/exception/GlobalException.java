824e706d12a513d1f34c504000b8476bdd2af0b0
leungandi <songzhangliang@foxmail.com>
2017-08-20 15:27:03 +0800

    增加全局异常

--- src/main/java/com/szl/springboot/exception/GlobalException.java
+++ src/main/java/com/szl/springboot/exception/GlobalException.java
@@ -1,6 +1,9 @@
 package com.szl.springboot.exception;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 /**
  * 全局异常处理
  * @author SongZhangLiang
@@ -8,10 +11,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @ControllerAdvice
 public class GlobalException {
+	private Logger logger =  Logger.getLogger(this.getClass());
 	
-	
-	public String error() {
-		return "error";
+	@ExceptionHandler(Exception.class)
+	public ModelAndView error(Exception e) {
+		logger.error("有错误出现,请及时解决",e);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("error", e.getMessage());
+		mv.setViewName("error500");
+		return mv;
 	}
 
 }
